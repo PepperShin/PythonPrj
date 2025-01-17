14508c7b29120b753030735b5593ff95d16c7b8c
PepperShin <peep1996@naver.com>
2025-01-17 17:46:29 +0900

    2025-01-17 after class

--- test2.py
+++ test2.py
@@ -1,2 +1,18 @@
+def add(a, b):
+    print("디버깅")
+    print("디버깅")
+    print("디버깅")
+
+    return a + b
+
+
+add(5, 6)
+
 print("Hello World2")
 print("헬로우 월드")
+print("디버깅")
+print("디버깅")
+print("디버깅")
+print("디버깅")
+print("디버깅")
+print("디버깅")
